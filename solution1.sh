22d018bbd879c86dde9753c684e208425634aa60
Dylan Enright <denri009@globus.ib.int.bioinfo.ucr.edu>
2018-10-10 10:32:04 -0700

    errors fixed

--- solution1.sh
+++ solution1.sh
@@ -4,13 +4,13 @@
 # The "curl" command doesn't work on my PC. It kicks back a security certificate error when working from GitHub. That's why I used wget
 
 wget --no-check-certificate  https://biodataprog.github.io/2018_programming-intro/data/Ecoli-vs-Yersinia.BLASTP.tab.gz > Yersinia_Ecoli_data.gz
-ls -l Ecoli-vs-Yersinia.BLASTP.tab.gz
+du -h Ecoli-vs-Yersinia.BLASTP.tab.gz
 gunzip Ecoli-vs-Yersinia.BLASTP.tab.gz
-ls -l Ecoli-vs-Yersinia.BLASTP.tab
+du -h Ecoli-vs-Yersinia.BLASTP.tab
 head -n 25 Ecoli-vs-Yersinia.BLASTP.tab
 tail -n 3 Ecoli-vs-Yersinia.BLASTP.tab
 wc Ecoli-vs-Yersinia.BLASTP.tab
-sort -k5n Nc3H.expr.tab > Nc20H.expr.sorted.tab
+sort -k6n Nc3H.expr.tab > Nc20H.expr.sorted.tab
 wget --no-check-certificate  https://biodataprog.github.io/2018_programming-intro/data/D_mel.63B12.gbk
 grep -c CDS D_mel.63B12.gbk
 cut -f3 Ecoli-vs-Yersinia.BLASTP.tab |grep 100
